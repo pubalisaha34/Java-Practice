bc878b3d557e71f7fb284c77aab0e7153a2f80a1
pubalisaha34 <139700070+pubalisaha34@users.noreply.github.com>
2025-12-28 14:01:37 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator{
+    public static void main(String[] args){
+        int  num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+
+        System.out.println("the sum is " + sum);
+
+        int diff = num1 - num2;
+
+        System.out.println("the diff is " + diff);
+
+
+
+
+    }
+}
